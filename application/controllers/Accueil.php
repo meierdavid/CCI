04d2ba748b85d09d74b79041f7c99adc18f893ab
CamilleThomasIG3 <camille.thomas@etu.umontpellier.fr>
2018-11-08 16:14:50 +0100

    Update application/controllers/Accueil.php

--- application/controllers/Accueil.php
+++ application/controllers/Accueil.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Accueil extends CI_Controller {
+class Accueil extends CI_Controller{
 
 	/**
 	 * Index Page for this controller.
