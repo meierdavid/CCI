9f9facfde4204da61c8a288464ee3d65bc97951e
CamilleThomasIG3 <camille.thomas@etu.umontpellier.fr>
2018-11-11 19:56:14 +0100

    inscription commercant

--- application/controllers/CommercantCtrl.php
+++ application/controllers/CommercantCtrl.php
@@ -19,9 +19,22 @@ class CommercantCtrl extends CI_Controller {
     }
         
         
-    public function inscription(){		
+    public function inscription(){
+        $this->load->model('commercant');		
 		$this->load->helper('form');
-		$this->load->view('commercant/inscription');   	
+		$this->load->view('commercant/inscription');
+        if(isset($_GET['mdpCommercant']) && $_GET['mdpCommercant']==$_GET['mdpCommercant2']){
+                $data=array(
+                            "prenomCommercant"=> htmlspecialchars($_GET['prenomCommercant']),
+                            "nomCommercant"=> htmlspecialchars($_GET['nomCommercant']),
+                            "mailCommercant" => htmlspecialchars($_GET['mailCommercant']),
+                            "mdpCommercant" => htmlspecialchars($_GET['mdpCommercant']),
+                            "adresseCommercant"=> htmlspecialchars($_GET['adresseCommercant']),
+                            "codePCommercant"=> htmlspecialchars($_GET['codePCommercant']),
+                            "villeCommercant" => htmlspecialchars($_GET['villeCommercant']),
+                            "telCommercant" => htmlspecialchars($_GET['telCommercant']),);  
+                $this->commercant->insert($data); 
+        }  	
 	}
 
 }
