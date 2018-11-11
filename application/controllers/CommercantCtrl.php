ca7e9c4376696fcc5fc7e596477c53655bed82db
CamilleThomasIG3 <camille.thomas@etu.umontpellier.fr>
2018-11-11 19:25:10 +0100

    crud commercant

---
+++ application/controllers/CommercantCtrl.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CommercantCtrl extends CI_Controller {
+
+	
+	public function index()
+	{       
+        $this->load->helper('url');
+        $this->load->view('commercant/profil');               
+	}
+
+    public function profil($id)
+    {
+        $data['commercant'] = $this->commercant->selectById($id);
+        $this->load->model('Commercant');
+        $this->load->helper('url');
+        $this->load->view('commercant/profil',$data);
+    }
+        
+        
+    public function inscription(){		
+		$this->load->helper('form');
+		$this->load->view('commercant/inscription');
+        var_dump($_GET);
+        die;    	
+	}
+
+}
