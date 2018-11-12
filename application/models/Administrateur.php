7cbc6847c11ac1874eaccebbfb41abb5b8358e3a
AmarAdam <adammegherat@gmail.com>
2018-11-12 10:52:42 +0100

    édentation + relecture code

--- application/models/Administrateur.php
+++ application/models/Administrateur.php
@@ -1,54 +1,70 @@
 <?php
 	class administrateur extends CI_Model{
+        
         protected $table ='administrateur';
-     	public function __construct() {
+     	
+        public function __construct() {
         	parent::__construct();
     	} 
-	public function selectAll(){
-		$this->load->database();
-    	return $this->db->select('*')
+
+        public function selectAll(){
+		
+        $this->load->database();
+    	
+            return $this->db->select('*')
                ->from('administrateur')
                ->get()
                ->result();
         }
+        
+
         public function selectById($id) {
-    		$this->load->database();
-    		return $this->db->select('*')
-                    ->from('administrateur')
-                    ->where('idAdministrateur', $id)
-                    ->get()
-                    ->result();
+    		
+            $this->load->database();
+    		
+            return $this->db->select('*')
+                ->from('administrateur')
+                ->where('idAdministrateur', $id)
+                ->get()
+                ->result();
             
   		}
+        
         public function selectByMail($mail) {
-    		$this->load->database();
-    		return $this->db->select('*')
+    		
+            $this->load->database();
+    		
+            return $this->db->select('*')
                     ->from('administrateur')
                     ->where('mailAdministrateur', $mail)
                     ->get()
                     ->result();
-            
-  		}
-  		public function getLastUserId() {
-    		$this->load->database();
-    		return $this->db->select('idAdministrateur')
+        }
+  		
+        public function getLastUserId() {
+    	
+        	$this->load->database();
+    	
+        	return $this->db->select('idAdministrateur')
         	        ->from('administrateur')
                     ->order_by('idAdministrateur', 'desc')
                     ->limit(1)
                     ->get()
                     ->result();
   		}
-    	public function insert($data) {
+    	
+        public function insert($data) {
+        
             $this->load->database();
             $this->db->set('prenomAdministrateur', $data['prenomAdministrateur'])
-        	->set('nomAdministrateur', $data['nomAdministrateur'])
- 		->set('mailAdministrateur', $data['mailAdministrateur'])
+                ->set('nomAdministrateur', $data['nomAdministrateur'])
+ 		        ->set('mailAdministrateur', $data['mailAdministrateur'])
                 ->set('adresseAdministrateur', $data['adresseAdministrateur'])
                 ->set('codePAdministrateur', $data['codePAdministrateur'])
                 ->set('villeAdministrateur', $data['villeAdministrateur'])
                 ->set('telAdministrateur', $data['telAdministrateur'])
                 ->set('mdpAdministrateur', $data['mdpAdministrateur'])
-		->insert($this->table);
+		      ->insert($this->table);
         }   
 			
 		public function delete($id){
@@ -58,10 +74,12 @@
     	}
         
         public function update($id, $data) {
+        
         $this->load->database();
+        
         $this->db->set('prenomAdministrateur', $data['prenomAdministrateur'])
-        	->set('nomAdministrateur', $data['nomAdministrateur'])
- 		->set('mailAdministrateur', $data['mailAdministrateur'])
+        	    ->set('nomAdministrateur', $data['nomAdministrateur'])
+ 		        ->set('mailAdministrateur', $data['mailAdministrateur'])
                 ->set('adresseAdministrateur', $data['adresseAdministrateur'])
                 ->set('codePAdministrateur', $data['codePAdministrateur'])
                 ->set('villeAdministrateur', $data['villeAdministrateur'])
