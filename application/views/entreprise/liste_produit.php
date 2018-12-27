cbfcf736a491a173b095d3b200f58dd0d303fc96
meierdavid <davidmeier@hotmail.fr>
2018-12-27 18:35:37 +0100

    liste produit

--- application/views/entreprise/liste_produit.php
+++ application/views/entreprise/liste_produit.php
@@ -18,8 +18,12 @@
                                             <tr>
                                                 <th scope="col">Nom</th>
                                                 <th scope="col">Description</th>
+                                                <th scope="col">Catégorie</th>
                                                 <th scope="col">Prix</th>
                                                 <th scope="col">Réduction</th>
+                                                <th scope="col">Ajouter</th>
+                                                <th scope="col">Supprimer</th>
+                                                <th scope="col">Modifier</th>
                                             </tr>
                                             </thead>
                                             <tbody>
@@ -30,6 +34,9 @@
                                                 <td><?php echo $item->categorieProduit; ?></td>
                                                 <td><?php echo $item->prixUnitaireProduit; ?></td>
                                                 <td><?php echo $item->reducProduit; ?></td>
+                                                <td><p><a href="<?php echo base_url("EntrepriseCtrl/ajouter_sous_produit/".$item->numSiret );?>">Ajouter un article</a></p></td>
+                                                <td><p><a href="<?php echo base_url("EntrepriseCtrl/supprimer_produit/".$item->numSiret );?>">Supprimer le produit</a></p></td>
+                                                <td><p><a href="<?php echo base_url("EntrepriseCtrl/modifier_produit/".$item->numSiret );?>">Supprimer le produit</a></p></td>
                                             </tr>
                                             <?php } ?>
                                             </tbody>
