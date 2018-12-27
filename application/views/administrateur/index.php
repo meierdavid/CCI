294598e11d573f6e42464ce4d556110fb92ee071
mathisbourrat <44777889+mathisbourrat@users.noreply.github.com>
2018-12-27 19:59:01 +0100

    liste_commercant

--- application/views/administrateur/index.php
+++ application/views/administrateur/index.php
@@ -18,6 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <a href="<?php echo base_url()?>AdministrateurCtrl/index">Accueil</a>
         <a href="<?php echo base_url()?>AdministrateurCtrl/ajout_administrateur">Ajouter un administrateur</a>
 		<a href="<?php echo base_url()?>AdministrateurCtrl/ajout_commercant">Ajouter un commercant</a>
+                <a href="<?php echo base_url()?>AdministrateurCtrl/liste_commercant">Liste des commercant</a>
         <a href="#" class="active">Votre Compte</a>
         <a href="<?php echo base_url()?>AdministrateurCtrl/changer_mdp">Changer mot de passe</a>
         <a href="<?php echo base_url()?>AdministrateurCtrl/supprimer_client">Supprimer un Client</a>
---
+++ application/views/administrateur/liste_client.php
@@ -0,0 +1,57 @@
+<div class="container">
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div>
+                <div style="margin-left: 60px" class="row">
+                    <div class="col-md-12 col-md-offset-2">
+                        <div class="box">
+                            <h2>Liste des clients</h2>
+                            <div class="row">
+                                <article class=" col-md-1 col-lg-1">
+
+
+                                </article>
+                                <article class=" col-md-11 col-lg-11">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped">
+                                            <thead>
+                                            <tr>
+                                                <th scope="col">Identifiant</th>
+                                                <th scope="col">Mail</th>
+                                                <th scope="col">Nom</th>
+                                                <th scope="col">Prénom</th>
+                                                <th scope="col">Supprimer</th>
+                                                <th scope="col">Modifier</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <?php foreach ($client as $item) { ?>
+                                                <td><?php echo $item->idClient; ?></td>
+                                                <td><?php echo $item->mailClient; ?></td>
+                                                <td><?php echo $item->nomClient; ?></td>
+                                                <td><?php echo $item->prenomClient; ?></td>
+                                                <td><p><a href="<?php echo base_url("AdministrateurCtrl/supprimer_client".$item->idCommercant);?>">Supprimer le Commerçant</a></p></td>
+                                                <td><p><a href="<?php echo base_url("AdministrateurCtrl/modifier_client".$item->idCommercant );?>">Modifier le commerçant</a></p></td>
+                                            </tr>
+                                            <?php } ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br></br>
+            <br></br>
+
+
+        </div>
+    </div>
+</div>
+
+
+
