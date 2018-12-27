c47dc9a743e9a849527cab6cac1946edfd9362b3
AmarAdam <adammegherat@gmail.com>
2018-12-27 17:28:33 +0100

    Update Panier.php

--- application/models/Panier.php
+++ application/models/Panier.php
@@ -1,8 +1,52 @@
 <?php
+    class panier extends CI_Model{
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        protected $table ='panier';
+        public function __construct() {
+            parent::__construct();
+        }
 
+        public function selectAll(){
+            $this->load->database();
+            return $this->db->select('*')
+                ->from('panier')
+                ->get()
+                ->result();
+        }
+
+        public function selectById($id) {
+            $this->load->database();
+            return $this->db->select('*')
+                ->from('panier')
+                ->where('idPanier', $id)
+                ->get()
+                ->result();
+
+        }
+
+        public function insert($data) {
+            $this->load->database();
+            $this->db->set('nomProduit', $data['nomProduit'])
+                ->set('descriptionProduit', $data['descriptionProduit'])
+                ->set('prixUnitaireProduit', $data['prixUnitaireProduit'])
+                ->set('reducProduit', $data['reducProduit'])
+                ->insert($this->table);
+        }
+
+        public function delete($id){
+            $this->load->database();
+            return $this->db->where('idPanier',$id)
+                ->delete($this->table);
+        }
+
+        public function update($id, $data) {
+            $this->load->database();
+            $this->db->set('nomProduit', $data['nomProduit'])
+                ->set('descriptionProduit', $data['descriptionProduit'])
+                ->set('prixUnitaireProduit', $data['prixUnitaireProduit'])
+                ->set('reducProduit', $data['reducProduit'])
+                ->update($this->table);
+        }
+
+    }
+?>
