ca15aa1bb8d2b2f74235d75814ab38e75202ce15
AmarAdam <adammegherat@gmail.com>
2019-01-02 18:28:54 +0100

    Update liste_panier.php

--- application/views/panier/liste_panier.php
+++ application/views/panier/liste_panier.php
@@ -16,31 +16,21 @@
                                         <table class="table table-striped">
                                             <thead>
                                             <tr>
-                                                <th scope="col">Nom</th>
-                                                <th scope="col">Description</th>
-                                                <th scope="col">Catégorie</th>
-                                                <th scope="col">Prix</th>
-                                                <th scope="col">Réduction</th>
-												<th scope="col">Couleur</th>
-												<th scope="col">Quantité Dispo</th>
-                                                <th scope="col">Ajouter</th>
-                                                <th scope="col">Supprimer</th>
-                                                <th scope="col">Modifier</th>
+                                                <th scope="col">date Panier</th>
+                                                <th scope="col">paiement Panier</th>
+                                                <th scope="col">code Promo appliqué</th>
+                                                <th scope="col">Prix total du panier</th>
+                                                <th scope="col">Etat</th>
                                             </tr>
                                             </thead>
                                             <tbody>
                                             <tr>
-                                                <?php foreach ($produits as $item) { ?>
-                                                <td><?php echo $item->nomProduit; ?></td>
-                                                <td><?php echo $item->descriptionProduit; ?></td>
-                                                <td><?php echo $item->categorieProduit; ?></td>
-                                                <td><?php echo $item->prixUnitaireProduit; ?></td>
-                                                <td><?php echo $item->reducProduit; ?></td>
-												<td><?php echo $item->couleurProduit; ?></td>
-												<td><?php echo $item->nbDispoProduit; ?></td>
-                                                <td><p><a href="<?php echo base_url("EntrepriseCtrl/ajouter_sous_produit/".$item->numSiret );?>">Ajouter un article</a></p></td>
-                                                <td><p><a href="<?php echo base_url("EntrepriseCtrl/supprimer_produit/".$item->numSiret );?>">Supprimer le produit</a></p></td>
-                                                <td><p><a href="<?php echo base_url("EntrepriseCtrl/modifier_produit/".$item->numSiret );?>">modifier  le produit</a></p></td>
+                                                <?php foreach ($panier as $item) { ?>
+                                                <td><?php echo $item->datePanier; ?></td>
+                                                <td><?php echo $item->paiementPanier; ?></td>
+                                                <td><?php echo $item->codePromo; ?></td>
+                                                <td><?php echo $item->prixTotPanier; ?></td>
+                                                <td><p><a href="<?php echo base_url("PanierCtrl/finaliser/".$item->finaliserPanier );?>">Finaliser</a></p></td>
                                             </tr>
                                             <?php } ?>
                                             </tbody>
