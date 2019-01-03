789d852e9d49970101a9c9552a3fca39471bbfb3
AmarAdam <adammegherat@gmail.com>
2019-01-04 00:08:21 +0100

    Create detail.php

---
+++ application/views/bonreduc/detail.php
@@ -0,0 +1,46 @@
+
+<!------ Include the above in your HEAD tag ---------->
+
+
+<!--Pulling Awesome Font -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-3 col-md-5">
+            <div class="form-login" style="margin-left: 200px;">
+                <?php echo form_open_multipart('BonReducCtrl/modifier'); ?>
+
+
+                <br></br>
+                <div class="form-group">
+                    <label class="control-label">idBon</label>
+                    <input type="text" class="form-control" name="idBon" value="<?php echo  $bonreduc[0]->idBon; ?>" size="30"/>
+                </div>
+                <div class="form-group">
+                    <label class="control-label">pourcentageBon</label>
+                    <input type="number" class="form-control" name="pourcentageBon" value="<?php echo  $bonreduc[0]->pourcentageBon; ?>" size="30"/>
+                </div>
+                <div class="form-group">
+                    <label class="control-label">numSiret</label>
+                    <input type="text" class="form-control" name="numSiret" value="<?php echo  $bonreduc[0]->numSiret; ?>" size="30" />
+                </div>
+                <div class="form-group">
+                    <label class="control-label">libelleBon</label>
+                    <input type="text" class="form-control" name="libelleBon" value="<?php echo  $bonreduc[0]->libelleBon; ?>" size="30" required/>
+                    <h6 style="color:red;"</h6>
+                </div>
+
+                <div class="text-center"><input class="btn btn-primary btn-success btn-block" type="submit" value="Modifier" /></div>
+                <div class="text-center">
+                    <br>
+                    <h1 style="color:darkslategrey; "></h1>
+                </div>
+                </form>
+                <br></br>
+                <br></br>
+
+            </div>
+        </div>
+    </div>
+</div>
