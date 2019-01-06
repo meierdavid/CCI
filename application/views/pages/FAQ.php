c7b77a822bbd12cd5e401ba483e6171db6bc0175
meierdavid <davidmeier@hotmail.fr>
2019-01-06 19:07:50 +0100

    Update FAQ.php

--- application/views/pages/FAQ.php
+++ application/views/pages/FAQ.php
@@ -26,12 +26,12 @@
 	        <!-- Question one -->
 	        <li>
 	            <div class="collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-	                Question 1 :
+	                Comment créditez son compte ?
 	                <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
 	            </div>
 	            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                   <div class="card-body">
-					Réponse 1
+                      Cliquez sur <strong>Profil</strong> ( en haut à gauche du site ) puis sur <strong>approvisionner mon compte</strong> 
 				</div>
                 </div>
 	        </li>
@@ -39,12 +39,14 @@
 	        <!-- Question two -->
 	        <li>
 	            <div class="collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-	                Question 2
+	                Comment utiliser mes points de fidélités ?
 	                <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
 	            </div>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                   <div class="card-body">
-				  Réponse 2
+                      Dans votre pannier cliquez sur le bouton <strong>utiliser mes points</strong>
+                      <br>
+                      Vous ne pouvez pas utiliser plus de <strong>50</strong> points d'un coup ce qui reduira le prix de votre panier de <strong>50%</strong>
                   </div>
                 </div>
 	        </li>
@@ -52,12 +54,14 @@
 	        <!-- Question three -->
 	        <li>
 	            <div class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-	               Question 3
+	               Comment valider que j'ai reçu une livraison ?
 	                <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
 	            </div>
                 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                   <div class="card-body">
-					Réponse 3                  
+                      Cliquez sur <strong>Historique des commandes</strong> (en haut du site)  puis sur <strong>Confirmer la reception</strong> 
+                      <br>
+                      Ainsi le commercant sera rémunéré à la hauteur du montant du produit
 					</div>
                 </div>
 	        </li>
@@ -65,12 +69,14 @@
 	        <!-- Question Four -->
 	        <li>
 	            <div class="collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-	                Question 4
+	                Comment voir les produits d'un seul magasin ?
 	                <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
 	            </div>
                 <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
                   <div class="card-body">
-					Réponse 4
+                      Vous pouvez cliquer dans <strong>L'accueil</strong> sur l'image <strong>Magasins</strong> puis selectioner celui qui vous interesse
+                      <br>
+                      Ou bien cliquer sur <strong>Catégories</strong> puis le nom du magasin dont vous chercher les produits
                   </div>
                 </div>
 	        </li>
@@ -78,28 +84,17 @@
 	        <!-- Questiion Five -->
 	        <li>
 	            <div class="collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-	                Question 5
+	                Où trouver des codes de réductions ?
 	                <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
 	            </div>
                 <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
                   <div class="card-body">
-					Réponse 5
+                      Vous pouvez cliquer sur <strong>Codes de réduction</strong> vous y trouverais tous les codes prévu par les magasins inscrit sur le site.
                   </div>
                 </div>
 	        </li>
 	        
-	        <!-- Question Six-->
-	        <li>
-	            <div class="collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-	                Question 6
-	                <span class="fa fa-chevron-down fa-1x text-info pull-right"></span>
-	            </div>
-                <div id="collapseSix" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                  <div class="card-body">
-					Réponse 6
-                  </div>
-                </div>
-	        </li>
+	       
 	    </ul>
 	</div>
 	
